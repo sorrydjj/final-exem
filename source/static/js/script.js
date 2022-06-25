d7df1305e7245d506168e29123bb2e6b8ce3af45
Ruslan <xenoncraftkg2@mail.ru>
2022-06-25 14:36:27 +0600

    Написан Js для раективного принятие или отклонения объявленией

---
+++ source/static/js/script.js
@@ -0,0 +1,57 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+async function acceptStatus(event) {
+    let pk = event.target.dataset.pk
+    let pathname = window.location.pathname
+    let element = document.getElementById("block_moderated_" + pk)
+    let csrftokens = getCookie('csrftoken');
+    let url = "/api/accept/announcemen/".replace(pathname, '')
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({"id": pk, "status": "accept"}),
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftokens
+        }
+    })
+    if (response.ok) {
+        element.remove()
+    } else {
+        alert("Операция не удалась")
+    }
+}
+
+async function rejectStatus(event) {
+    let pk = event.target.dataset.pk
+    let pathname = window.location.pathname
+    let element = document.getElementById("block_moderated_" + pk)
+    let csrftokens = getCookie('csrftoken');
+    let url = "/api/reject/announcemen/".replace(pathname, '')
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({"id": pk, "status": "reject"}),
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftokens
+        }
+    })
+    if (response.ok) {
+        element.remove()
+    } else {
+        alert("Операция не удалась")
+    }
+}
--- source/webapp/views/api.py
+++ source/webapp/views/api.py
@@ -8,22 +8,24 @@ from webapp.models import Announcement
 
 
 def set_status_accept(request):
-    try:
-        if request.body:
-            body = json.loads(request.body)
-            if body["status"] == "accept":
-                Announcement.objects.get(pk=body["id"]).set_accept_status()
-                return JsonResponse(data={"status": "success"}, status=http.HTTPStatus.OK)
-    except:
-        return JsonResponse(data={"status": "failed"}, status=http.HTTPStatus.NOT_FOUND)
+    if request.method == "POST" and request.user.is_staff:
+        try:
+            if request.body:
+                body = json.loads(request.body)
+                if body["status"] == "accept":
+                    Announcement.objects.get(pk=body["id"]).set_accept_status()
+                    return JsonResponse(data={"status": "success"}, status=http.HTTPStatus.OK)
+        except:
+            return JsonResponse(data={"status": "failed"}, status=http.HTTPStatus.NOT_FOUND)
 
 
 def set_status_reject(request):
-    try:
-        if request.body:
-            body = json.loads(request.body)
-            if body["status"] == "reject":
-                Announcement.objects.get(pk=body["id"]).set_reject_status()
-                return JsonResponse(data={"status": "success"}, status=http.HTTPStatus.OK)
-    except:
-        return JsonResponse(data={"status": "failed"}, status=http.HTTPStatus.NOT_FOUND)
+    if request.method == "POST" and request.user.is_staff:
+        try:
+            if request.body:
+                body = json.loads(request.body)
+                if body["status"] == "reject":
+                    Announcement.objects.get(pk=body["id"]).set_reject_status()
+                    return JsonResponse(data={"status": "success"}, status=http.HTTPStatus.OK)
+        except:
+            return JsonResponse(data={"status": "failed"}, status=http.HTTPStatus.NOT_FOUND)
