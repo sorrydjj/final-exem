4cd49ce762c8ef8d0b10334d66c5a1e71a72ded7
Ruslan <xenoncraftkg2@mail.ru>
2022-06-25 15:11:15 +0600

    Исправлена ошибка переадресации в удаление объявления

--- source/webapp/views/announcement.py
+++ source/webapp/views/announcement.py
@@ -1,10 +1,6 @@
-import datetime
-
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.db.models import Q
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
-from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from webapp.forms import AnnouncementCreateForm, SearchForm
@@ -47,7 +43,6 @@ class AnnouncementListView(ListView):
             return self.form.cleaned_data.get("search")
 
 
-
 class AnnouncementCreateView(LoginRequiredMixin, CreateView):
     model = Announcement
     template_name = "../templates/announcement/announcement_create.html"
@@ -56,7 +51,7 @@ class AnnouncementCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.save()
-        return redirect("webapp:announcemen_detail", pk=form.instance.pk)
+        return redirect("webapp:announcemens_list")
 
 
 class AnnouncementUpdateView(PermissionRequiredMixin, UpdateView):
@@ -88,10 +83,7 @@ class AnnouncementDeleteView(PermissionRequiredMixin, DeleteView):
     def form_valid(self, form):
         self.object.is_delete = True
         self.object.save()
-        return HttpResponseRedirect(self.success_url)
-
-    def get_success_url(self):
-        return reverse("webapp:announcemens_list")
+        return redirect("webapp:announcemens_list")
 
     def has_permission(self):
         return self.request.user == self.get_object().author
@@ -102,9 +94,37 @@ class AnnouncementModeratedListView(PermissionRequiredMixin, ListView):
     template_name = "../templates/announcement/moderated_list.html"
     context_object_name = "announcements"
     permission_required = "webapp.view_announcement"
+    paginate_by = 9
+    paginate_orphans = 0
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
-        return super().get_queryset().filter(status="moderated").exclude(is_delete=True).order_by("created_at")
+        queryset = super().get_queryset().filter(status="moderated").exclude(is_delete=True).order_by("created_at")
+        if self.search_value:
+            print(self.search_value)
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = SearchForm()
+        if self.search_value:
+            context['form'] = SearchForm(initial={"search": self.search_value})
+            context['search'] = self.search_value
+        return context
+
+    def get_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get("search")
+
 
 
 class AnnouncementModeratedDetailView(PermissionRequiredMixin, DetailView):
